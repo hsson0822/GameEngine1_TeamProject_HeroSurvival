de640058fec30f9e56e46a8e43f22992e6351f32
hsson0822 <hsson0822@naver.com>
2022-05-16 01:12:29 +0900

    몬스터 웨이브 기능 추가

--- Assets/Scripts/InGameManager.cs
+++ Assets/Scripts/InGameManager.cs
@@ -34,16 +34,13 @@ public class InGameManager : MonoBehaviour
 
         StartCoroutine(EnemyControl());
 
-        var position = new Vector3(Random.Range(50f, 100f), 1f, Random.Range(50f, 100f));
-        var rotation = new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f));
-        poolingManager.Get("Enemy1", position, Quaternion.Euler(rotation));
     }
 
     void FixedUpdate()
     {
         count += Time.deltaTime;
 
-        if(count > itemTime)
+        if (count > itemTime)
         {
             float x = Random.Range(player.transform.position.x - 100.0f, player.transform.position.x + 100.0f);
             float z = Random.Range(player.transform.position.z - 100.0f, player.transform.position.z + 100.0f);
@@ -74,28 +71,53 @@ public class InGameManager : MonoBehaviour
 
             if (time > 50.0f)
             {
-                switch(index)
-                {
-                    case 0:
-                        break;
+                //switch (index)
+                //{
+                //    case 0:
+                //        Wave();
+                //        break;
 
-                    case 1:
-                        break;
+                //    case 1:
+                //        break;
 
-                    case 2:
-                        break;
+                //    case 2:
+                //        break;
 
-                    case 3:
-                        break;
+                //    case 3:
+                //        break;
 
-                    case 4:
-                        break;
-                }
+                //    case 4:
+                //        break;
+                //}
+                Wave();
                 ++index;
                 time = 0.0f;
             }
 
             yield return null;
+        }
+    }
+
+    void Wave()
+    {
+        for (int i = 0; i < 30; ++i)
+        {
+            var rotation = new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f));
+
+            float radius = 30f;
+            Vector3 playerPosition = player.transform.position;
+
+            float a = playerPosition.x;
+            float b = playerPosition.z;
+
+            float x = Random.Range(-radius + a, radius + a);
+            float z_b = Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow(x - a, 2));
+            z_b *= Random.Range(0, 2) == 0 ? -1 : 1;
+            float z = z_b + b;
+
+            Vector3 randomPosition = new Vector3(x, 1.0f, z);
+            poolingManager.Get("Enemy1", randomPosition, Quaternion.Euler(rotation));
+
         }
     }
 }
