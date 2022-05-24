83a54ef80f2cba8732a2af421d48c2492e529ced
Sunghyun Park <touch0315@naver.com>
2022-05-24 12:52:53 +0900

    적 에셋추가
    Log, Bee, Mini_Golem

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//투사체 회전 
 public class Item : MonoBehaviour
 {
     public float rotateSpeed;
     void Update()
     {
-        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
     }
 }
